f893f666b6b8664e2078cd0119f76337e884abaa
tanocarfi <tanocarfi@gmail.com>
2019-11-22 10:46:33 +0100

    added new students features: getAll and add.

--- src/models/student.ts
+++ src/models/student.ts
@@ -2,6 +2,7 @@ import mongoose, { Schema } from "mongoose";
 import uniqueValidator from "mongoose-unique-validator";
 import fiscalCodeSchema from "./helpers/person.schema";
 import personSchema from "./helpers/person.schema";
+import Student from "./interfaces/student";
 
 const studentSchema = new Schema({
   ...personSchema
@@ -10,3 +11,24 @@ const studentSchema = new Schema({
 studentSchema.plugin(uniqueValidator);
 
 export const StudentModel = mongoose.model("Student", studentSchema);
+
+// insert method
+
+export const getAll = async () => {
+  try {
+    const students = await StudentModel.find();
+    return students === null ? new Error("No student found!") : students;
+  } catch (err) {
+    return new Error(`Error: ${err}`);
+  }
+}
+
+export const add = async (stud: Student) => {
+  try {
+    const students = new StudentModel(stud);
+    await students.save();
+    return students;
+  } catch (err) {
+    return new Error(`Error: ${err}`);
+  }
+}
--- src/routes/students.ts
+++ src/routes/students.ts
@@ -1,17 +1,13 @@
 import express from "express";
+import { StudentModel, getAll, add } from "../models/student";
+import Student from "../models/interfaces/student";
 
 const router = express.Router();
 
-const dummyStundets: Object[] = [
-  {
-    name: "Gaetano",
-    surname: "Carfì",
-    age: "21"
-  }
-];
-
 router.get("/", (req, res, next) => {
-  res.status(200).json();
+  getAll().then((student) => {
+    res.status(200).json(student);
+  });
 });
 
 router.get("/:id", (req, res, next) => {
@@ -23,7 +19,11 @@ router.delete("/:id", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  res.status(200).json();
+  const { fiscalCode, name, surname, dateOfBirth } = req.body;
+  const stud: Student = { fiscalCode, name, surname, dateOfBirth };
+  add(stud).then((stud) => {
+    res.status(201).json(stud);
+  });
 });
 
 router.put("/:id", (req, res, next) => {

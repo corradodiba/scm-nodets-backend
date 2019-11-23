dcaecea137ef024e10bf8bbe49c278cd44ef3a2c
tanocarfi <tanocarfi@gmail.com>
2019-11-23 09:06:01 +0100

    Added async function and bugfix

--- src/routes/students.ts
+++ src/routes/students.ts
@@ -1,33 +1,85 @@
 import express from "express";
-import { StudentModel, getAll, add } from "../models/student";
+import { StudentModel, getAll, add, deleteStudent, modifyStudent, getStudentById } from "../models/student";
 import Student from "../models/interfaces/student";
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  getAll().then((student) => {
-    res.status(200).json(student);
-  });
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await getAll();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(404).json({ message: err });
+  }
 });
 
-router.get("/:id", (req, res, next) => {
-  res.status(200).json();
+router.get("/:id", async (req, res, next) => {
+  try {
+    const result: any = await getStudentById(req.params.id);
+    res.status(200).json({
+      _id: result._id,
+      fiscalCode: result.fiscalCode,
+      name: result.name,
+      surname: result.surname,
+      dateOfBirth: result.dateOfBirth
+    });
+  } catch (err) {
+    res.status(404).json({ message: err });
+  }
 });
 
-router.delete("/:id", (req, res, next) => {
-  res.status(200).json();
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const result: any = await deleteStudent(req.params.id);
+    res.status(201).json({
+      message: "Student successfully deleted",
+      student: {
+        fiscalCode: result.fiscalCode,
+        name: result.name,
+        surname: result.surname,
+        dateOfBirth: result.dateOfBirth
+      }
+    });
+  } catch (err) {
+    res.status(404).json({ message: err });
+  }
 });
 
-router.post("/", (req, res, next) => {
-  const { fiscalCode, name, surname, dateOfBirth } = req.body;
-  const stud: Student = { fiscalCode, name, surname, dateOfBirth };
-  add(stud).then((stud) => {
-    res.status(201).json(stud);
-  });
+router.post("/", async (req, res, next) => {
+  try {
+    const { fiscalCode, name, surname, dateOfBirth } = req.body;
+    const stud: Student = { fiscalCode, name, surname, dateOfBirth };
+    const result: any = await add(stud);
+    res.status(201).json({
+      _id: result._id,
+      fiscalCode: result.fiscalCode,
+      name: result.name,
+      surname: result.surname,
+      dateOfBirth: result.dateOfBirth
+    });
+  } catch (err) {
+    res.status(404).json({ message: err });
+  }
 });
 
-router.put("/:id", (req, res, next) => {
-  res.status(200).json();
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { fiscalCode, name, surname, dateOfBirth } = req.body;
+    const stud: Student = { fiscalCode, name, surname, dateOfBirth };
+    const result: any = await modifyStudent(req.params.id, stud);
+    res.status(201).json({
+      message: "Subject successfully edited!",
+      before: {
+        fiscalCode: result.fiscalCode,
+        name: result.name,
+        surname: result.surname,
+        dateOfBirth: result.dateOfBirth
+      },
+      after: { fiscalCode, name, surname, dateOfBirth }
+    });
+  } catch (err) {
+    res.status(404).json({ message: err });
+  }
 });
 
 export default router;

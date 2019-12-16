83dbe4a36ac8278b03da3bc612605e547ce4f499
Corrado Dibattista <corradodiba@gmail.com>
2019-12-16 13:00:01 +0100

    created users helper components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -8,9 +8,11 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { MatToolbarModule } from "@angular/material";
+import { UsersListsComponent } from './users/users-lists/users-lists.component';
+import { UserComponent } from './users/user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent, NavbarComponent],
+  declarations: [AppComponent, AuthComponent, NavbarComponent, UsersListsComponent, UserComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ front-end/src/app/users/users-lists/users-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-lists',
+  templateUrl: './users-lists.component.html',
+  styleUrls: ['./users-lists.component.scss']
+})
+export class UsersListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

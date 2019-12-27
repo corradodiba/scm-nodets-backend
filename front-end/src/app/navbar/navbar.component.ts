94f8d82bca8de0d1f9f4361ac93bde398ec5f6a6
Corrado Dibattista <corradodiba@gmail.com>
2019-12-27 18:56:38 +0100

    added logout function in navbar component

--- front-end/src/app/navbar/navbar.component.ts
+++ front-end/src/app/navbar/navbar.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
 
+import { AuthService } from "../auth/auth.service";
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  private authListenerSubs = new Subscription();
+  isAuthenticated = false;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
+    this.isAuthenticated = this.authService.isAuth();
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.isAuthenticated = isAuthenticated;
+      });
   }
 
+  onLogout() {
+    this.authService.logoutUser();
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
 }

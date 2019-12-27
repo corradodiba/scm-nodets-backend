e33350ced28233a7ecdc46e593bc6c1cdab0ae9f
Corrado Dibattista <corradodiba@gmail.com>
2019-12-27 20:57:01 +0100

    added authentication cache system

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "./auth/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent implements OnInit {
+  title = "SocialNetwork";
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.autoConfigAuthUser();
+  }
 }

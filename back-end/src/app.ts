f843827346039dc8fa3a911c487b10b8de8f0859
Corrado Dibattista <corradodiba@gmail.com>
2019-12-29 00:00:48 +0100

    fixed authentication bug in backend

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -10,8 +10,8 @@ import { isAuth } from "./middlewares/isAuth";
 
 const PORT = 3000 || process.env.PORT;
 const MONGO_CLUSTER_URL =
-  // "mongodb+srv://admin_class-managing:QHcojgdSyqrOCb7y@stevejobs-csiyz.mongodb.net/class-managing?retryWrites=true&w=majority";
-  "mongodb://localhost:27017/class-managing";
+  "mongodb+srv://admin_class-managing:QHcojgdSyqrOCb7y@stevejobs-csiyz.mongodb.net/class-managing?retryWrites=true&w=majority";
+// "mongodb://localhost:27017/class-managing";
 
 const app = express();
 
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/courses", isAuth, coursesRoutes);
+app.use("/courses", coursesRoutes);
 app.use("/users", usersRoutes);
 app.use("/subjects", subjectsRoutes);
 app.use("/auth", authRoutes);

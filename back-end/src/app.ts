5d22dfe9d2f206ab3980d8c59b96c7116bd111cf
Corrado Dibattista <corradodiba@gmail.com>
2019-12-28 23:01:00 +0100

    added scaffolding for java android application

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -6,12 +6,11 @@ import usersRoutes from "./routes/users";
 import subjectsRoutes from "./routes/subjects";
 import authRoutes from "./routes/auth";
 import coursesRoutes from "./routes/courses";
-import { isAuth } from "./middlewares/isAuth";
 
 const PORT = 3000 || process.env.PORT;
 const MONGO_CLUSTER_URL =
-  // "mongodb+srv://admin_class-managing:QHcojgdSyqrOCb7y@stevejobs-csiyz.mongodb.net/class-managing?retryWrites=true&w=majority";
-  "mongodb://localhost:27017/class-managing";
+  "mongodb+srv://admin_class-managing:QHcojgdSyqrOCb7y@stevejobs-csiyz.mongodb.net/class-managing?retryWrites=true&w=majority";
+// "mongodb://localhost:27017/class-managing";
 
 const app = express();
 
@@ -30,7 +29,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/courses", isAuth, coursesRoutes);
+app.use("/courses", coursesRoutes);
 app.use("/users", usersRoutes);
 app.use("/subjects", subjectsRoutes);
 app.use("/auth", authRoutes);

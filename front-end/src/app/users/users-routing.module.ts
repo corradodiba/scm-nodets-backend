aa7e2cd736794c08d14c89b364bb2d78a1d91c0c
Corrado Dibattista <corradodiba@gmail.com>
2019-12-30 23:18:10 +0100

    added definitive settings to backend, and fixed navigation in personal profile for the students and teachers

--- front-end/src/app/users/users-routing.module.ts
+++ front-end/src/app/users/users-routing.module.ts
@@ -3,10 +3,16 @@ import { RouterModule, Routes } from "@angular/router";
 import { AdminComponent } from "./admin/admin.component";
 import { UsersListsComponent } from "./users-lists/users-lists.component";
 import { IsAdminGuard } from "../auth/permissions/isAdmin.guard";
+import { IsStudentGuard } from "../auth/permissions/isStudent.guard";
+import { IsTeacherGuard } from "../auth/permissions/isTeacher.guard";
 
 const routes: Routes = [
   { path: "", component: UsersListsComponent, canActivate: [IsAdminGuard] },
-  { path: ":userId", component: AdminComponent, canActivate: [IsAdminGuard] }
+  {
+    path: ":userId",
+    component: AdminComponent,
+    canActivate: [IsStudentGuard] || [IsTeacherGuard] || [IsAdminGuard]
+  }
 ];
 
 @NgModule({

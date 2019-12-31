6c9f7204fa3136fcb1f6289aa1e023a7ae6bbf1e
gabrielezagarella <gabry-zaga20@hotmail.it>
2019-12-31 18:14:51 +0100

    login completed

--- android/app/src/main/java/com/demo/stevejobsclassmanaging/Login.java
+++ android/app/src/main/java/com/demo/stevejobsclassmanaging/Login.java
@@ -4,19 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Login extends AppCompatActivity {
 
         EditText email, password;
         Button login;
         TextView register;
-        boolean isUsernameValid, isPasswordValid;
+        boolean isEmailValid, isPasswordValid;
         TextInputLayout emailError, passError;
+        private RequestQueue requestQueue;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -46,17 +60,15 @@ public class Login extends AppCompatActivity {
             });
         }
         public void LoginValidation() {
-            String emails = "gabriele";
-            String pass = "gabrielezagarella";
 
             if (email.getText().toString().isEmpty()) {
                 emailError.setError(getResources().getString(R.string.email_error));
-                isUsernameValid = false;
-            } else if (!email.getText().toString().equals(emails)){
+                isEmailValid = false;
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
                 emailError.setError(getResources().getString(R.string.error_invalid_email));
-                isUsernameValid = false;
+                isEmailValid = false;
             } else  {
-                isUsernameValid = true;
+                isEmailValid = true;
                 emailError.setErrorEnabled(false);
             }
 
@@ -66,18 +78,48 @@ public class Login extends AppCompatActivity {
             } else if (password.getText().length() < 8) {
                 passError.setError(getResources().getString(R.string.error_invalid_password));
                 isPasswordValid = false;
-            } else if (!password.getText().toString().equals(pass)) {
-                passError.setError( getResources().getString( R.string.error_incorrect_password ));
-                isPasswordValid = false;
             } else {
                 isPasswordValid = true;
                 passError.setErrorEnabled(false);
             }
 
-            if (isUsernameValid  && isPasswordValid) {
-                Toast.makeText(getApplicationContext(), "Login successfully", Toast.LENGTH_SHORT).show();
-                final Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                startActivity(intent);
+            if (isEmailValid  && isPasswordValid) {
+                jsonrequestSubjects();
             }
         }
+        private void jsonrequestSubjects() {
+        JSONObject jsonObject = new JSONObject();
+        final String emails = email.getText().toString();
+        final String passwords = password.getText().toString();
+        try {
+            jsonObject.put( "email", emails );
+            jsonObject.put( "password", passwords );
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String URLSIGNUP = "http://192.168.43.156:3000/auth/login";
+
+        requestQueue = Volley.newRequestQueue( Login.this);
+
+        JsonObjectRequest subject = new JsonObjectRequest( Request.Method.POST, URLSIGNUP, jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //text.setText(response.toString());
+                        Toast.makeText( Login.this, "Login succesfully", Toast.LENGTH_SHORT ).show();
+                        final Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                        startActivity(intent);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //text.setText( "Post: Response failled" );
+                VolleyLog.d( "Error: ", error.getMessage() );
+                error.printStackTrace();
+                Toast.makeText( Login.this, error.getMessage(), Toast.LENGTH_SHORT ).show();
+            }
+        } );
+        requestQueue.add( subject );
     }
+}

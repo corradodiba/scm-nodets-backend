79a8f14e7be4b3d4f50b13db52f9f2a067c0abe4
tanocarfi <tanocarfi@gmail.com>
2020-01-03 18:44:43 +0100

    added teacher dashboard

--- front-end/src/app/templates/teacher/teacher-dashboards/teacher-dashboards.module.ts
+++ front-end/src/app/templates/teacher/teacher-dashboards/teacher-dashboards.module.ts
@@ -3,9 +3,24 @@ import { CommonModule } from "@angular/common";
 
 import { TeacherDashboardsComponent } from "./teacher-dashboards.component";
 
+import { SidenavModule } from 'src/app/shared/sidenav/sidenav.module';
+import { ProfileNavbarModule } from 'src/app/shared/profile-navbar/profile-navbar.module';
+import { HeaderNavModule } from 'src/app/components/header-nav/header-nav.module';
+import { CardModule } from 'src/app/components/card/card.module';
+import { SimpleListModule } from 'src/app/components/simple-list/simple-list.module';
+import { FooterModule } from 'src/app/shared/footer/footer.module';
+
 @NgModule({
   declarations: [TeacherDashboardsComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SidenavModule,
+    ProfileNavbarModule,
+    HeaderNavModule,
+    CardModule,
+    SimpleListModule,
+    FooterModule
+  ],
   exports: [TeacherDashboardsComponent]
 })
 export class TeacherDashboardsModule {}

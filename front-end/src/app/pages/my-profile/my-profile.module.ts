26d1db507507c4c113cc8bb1ebb6522282349b9e
tanocarfi <tanocarfi@gmail.com>
2020-01-04 12:22:54 +0100

    added remaining users profiles

--- front-end/src/app/pages/my-profile/my-profile.module.ts
+++ front-end/src/app/pages/my-profile/my-profile.module.ts
@@ -5,10 +5,12 @@ import { MyProfileComponent } from "./my-profile.component";
 
 import { SidenavModule } from "src/app/shared/sidenav/sidenav.module";
 import { AdminMyProfileModule } from "src/app/templates/admin/my-profile/admin-my-profile.module";
+import { TeacherMyProfileModule } from "src/app/templates/teacher/teacher-my-profile/teacher-my-profile.module";
+import { StudentMyProfileModule } from "src/app/templates/student/student-my-profile/student-my-profile.module";
 
 @NgModule({
   declarations: [MyProfileComponent],
-  imports: [CommonModule, SidenavModule, AdminMyProfileModule],
+  imports: [CommonModule, SidenavModule, AdminMyProfileModule, TeacherMyProfileModule, StudentMyProfileModule],
   exports: [MyProfileComponent]
 })
 export class MyProfileModule {}
--- front-end/src/app/templates/student/dashboards/student-dashboards.component.ts
+++ front-end/src/app/templates/student/dashboards/student-dashboards.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from "rxjs";
+
+import Course from "src/app/interfaces/course.model";
+import { ISimpleList } from "src/app/interfaces/new/simple-list.model";
+
+import { CoursesService } from "src/app/pages/courses/courses.service";
+import { AuthService } from "src/app/pages/auth/auth.service";
 
 @Component({
   selector: 'app-student-dashboards',
@@ -6,10 +13,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-dashboards.component.scss']
 })
 export class StudentDashboardsComponent implements OnInit {
+  private authListenerSubs = new Subscription();
+  isAuthenticated = false;
 
-  constructor() { }
+  subjectsSimpleList: ISimpleList;
 
-  ngOnInit() {
+  course: Course;
+
+  constructor(
+    private authService: AuthService,
+    private coursesService: CoursesService
+  ) {}
+
+  async ngOnInit() {
+    this.isAuthenticated = this.authService.isAuth();
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.isAuthenticated = isAuthenticated;
+      });
+    if (this.isAuthenticated) {
+      this.course = await this.coursesService.getCourseById(
+        "5e0e6fa9fa991b5764169042"
+      );
+      this.subjectsSimpleList = this.getSubjectsSimpleListAssets();
+    }
+  }
+
+  getSubjectsSimpleListAssets() {
+    return {
+      title: "Subjects",
+      subtitle: `${this.course.subjects.length} subjects into course ${this.course.name}`,
+      items: this.course.subjects.map(subject => {
+        return { avatar: `${subject.name.charAt(0)}`, text: subject.name };
+      })
+    };
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
   }
 
 }
